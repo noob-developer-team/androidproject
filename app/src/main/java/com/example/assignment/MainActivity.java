aa002607fd96ea3c511e8f30031cb7cc84aed9c5
ThisIsFeeder <45194921+ThisIsFeeder@users.noreply.github.com>
2019-06-07 18:26:52 +0700

    commit

---
+++ app/src/main/java/com/example/assignment/MainActivity.java
@@ -0,0 +1,65 @@
+package com.example.assignment;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.android.volley.Request;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+
+import java.util.HashMap;
+import java.util.Map;
+
+
+public class MainActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        final EditText txtUsername = findViewById(R.id.txtUsername);
+        final EditText txtPassword = findViewById(R.id.txtPassword);
+        Button btnLogin = findViewById(R.id.btnLogin);
+
+        btnLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String url = "http://192.168.168.9:8181/HelloWorld/Text.php";
+                StringRequest stringRequest = new StringRequest(Request.Method.POST,url, new Response.Listener<String>() {
+                    @Override
+                    public void onResponse(String response) {
+                        if(response.contains("success_login"))
+                        {
+                            Toast.makeText(getApplicationContext(), response, Toast.LENGTH_SHORT).show();
+                        }
+                        else
+                        {
+                            Toast.makeText(getApplicationContext(), "SHIT", Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                }, new Response.ErrorListener(){
+                    @Override
+                    public void onErrorResponse(VolleyError error)
+                    {
+                        Toast.makeText(getApplicationContext(),error.toString(), Toast.LENGTH_LONG).show();
+                    }
+                }){
+                    @Override
+                    protected Map<String, String> getParams(){
+                        Map<String, String> params = new HashMap<>();
+                        params.put("username", txtUsername.getText().toString());
+                        params.put("password", txtPassword.getText().toString());
+                        return params;
+                    }
+                };
+                MySingleton.getInstance(getApplicationContext()).addToRequestQueue(stringRequest);
+            }
+        });
+//        MySingleton.getInstance(this).addToRequestQueue(jsonObjectRequest);
+    }
+}
